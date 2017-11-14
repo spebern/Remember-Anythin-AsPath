75aa2cfcc01e0d712c73db15938737aff2da6b02
Bernhard Specht <bernhard@specht.net>
2017-11-14 13:06:57 +0100

    create dir if it doesn't exist

--- lib/Remember/Anything/AsPath.pm
+++ lib/Remember/Anything/AsPath.pm
@@ -31,7 +31,8 @@ sub new {
 
     $args{out_dir} ||= '.';
     $args{out_dir} =~ s{[\/\\]$}{}; # remove trailing slash or backslash
-    croak 'out dir does not exist' unless -d $args{out_dir};
+    eval { mkdir $args{out_dir} unless -d $args{out_dir} }
+        || croak 'could not create dir: "' . $args{out_dir} . '"';
 
     return bless \%args, $class;
 }
